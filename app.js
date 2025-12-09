17e246c8e4ea2616ce1a57ed69ca9211c5918e03
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-09 06:53:58 +0530

    Fixed off-by-one condition for active light index

--- app.js
+++ app.js
@@ -14,3 +14,4 @@ function changeColor(){
 body.style.backgroundColor = getComputedStyle(lights[active]).getPropertyValue('--light-color');
   
   lights[active].classList.add('active');
+ if(active === lights.length -1){
